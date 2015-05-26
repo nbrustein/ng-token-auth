e4d35ab5f985a522f447d955cb6532b4fec4e6f4
ann_lewis <ann@pedago.com>
2015-05-26 18:35:48 -0400

    fix(sessions): fix 500 error when logging in with bad password by making update_auth_header run after session#new only when were returning a 200

--- app/controllers/devise_token_auth/concerns/set_user_by_token.rb
+++ app/controllers/devise_token_auth/concerns/set_user_by_token.rb
@@ -14,7 +14,6 @@ module DeviseTokenAuth::Concerns::SetUserByToken
 
   # user auth
   def set_user_by_token(mapping=nil)
-
     # determine target authentication class
     rc = resource_class(mapping)
 
@@ -55,41 +54,42 @@ module DeviseTokenAuth::Concerns::SetUserByToken
 
 
   def update_auth_header
+    if response.code == '200'
 
-    # cannot save object if model has invalid params
-    return unless @resource and @resource.valid? and @client_id
+      # cannot save object if model has invalid params
+      return unless @resource and @resource.valid? and @client_id
 
-    # Lock the user record during any auth_header updates to ensure
-    # we don't have write contention from multiple threads
-    @resource.with_lock do
+      # Lock the user record during any auth_header updates to ensure
+      # we don't have write contention from multiple threads
+      @resource.with_lock do
 
-      # determine batch request status after request processing, in case
-      # another processes has updated it during that processing
-      @is_batch_request = is_batch_request?(@resource, @client_id)
+        # determine batch request status after request processing, in case
+        # another processes has updated it during that processing
+        @is_batch_request = is_batch_request?(@resource, @client_id)
 
-      auth_header = {}
+        auth_header = {}
 
-      if not DeviseTokenAuth.change_headers_on_each_request
-        auth_header = @resource.build_auth_header(@token, @client_id)
+        if not DeviseTokenAuth.change_headers_on_each_request
+          auth_header = @resource.build_auth_header(@token, @client_id)
 
-        # update the response header
-        response.headers.merge!(auth_header)
+          # update the response header
+          response.headers.merge!(auth_header)
 
-      # extend expiration of batch buffer to account for the duration of
-      # this request
-      elsif @is_batch_request
-        auth_header = @resource.extend_batch_buffer(@token, @client_id)
+        # extend expiration of batch buffer to account for the duration of
+        # this request
+        elsif @is_batch_request
+          auth_header = @resource.extend_batch_buffer(@token, @client_id)
 
-      # update Authorization response header with new token
-      else
-        auth_header = @resource.create_new_auth_token(@client_id)
+        # update Authorization response header with new token
+        else
+          auth_header = @resource.create_new_auth_token(@client_id)
 
-        # update the response header
-        response.headers.merge!(auth_header)
-      end
+          # update the response header
+          response.headers.merge!(auth_header)
+        end
 
-    end # end lock
-
+      end # end lock
+    end
   end
 
   def resource_class(m=nil)
